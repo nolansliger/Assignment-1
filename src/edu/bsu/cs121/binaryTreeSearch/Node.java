1cf4a15dd53a9d117f9de564b3a23075760a74ff
slig12 <nrsliger@bsu.edu>
2017-01-16 19:19:24 -0500

    Update Node.java

--- src/edu/bsu/cs121/binaryTreeSearch/Node.java
+++ src/edu/bsu/cs121/binaryTreeSearch/Node.java
@@ -2,35 +2,37 @@ package edu.bsu.cs121.binaryTreeSearch;
 
 import java.util.ArrayList;
 
-//Author: Dr. Jeff Zhang and Mr. Aaron Feng
-public class Node <T> {
-	private Node <T> left;
-	private Node <T> right;
+public class TreeNode <T> {
+	//Changed Node to TreeNode.  More descriptive class name 
+	private Node <T> leftNode;
+	//changed left to leftNode, more descriptive
+	private Node <T> rightNode;
+	//same as the leftNode
 	private T element;
 /**
 	    * Creates a new tree node with the specified data.
 	    *
 	    * @param obj  the element that will become a part of the new tree node
  */
-	public Node ( T obj){
-		left=null;
-		right=null;
+	public TreeNode ( T obj){
+		leftNode=null;
+		rightNode=null;
 		element=obj;
 	}
-	public Node<T> getLeft() {
-		return left;
+	public TreeNode<T> getLeft() {
+		return leftNode;
 	}
 
-	public void setLeft(Node<T> left) {
-		this.left = left;
+	public void setLeft(Node<T> leftNode) {
+		this.left = leftNode;
 	}
 
 	public Node<T> getRight() {
-		return right;
+		return rightNode;
 	}
 
-	public void setRight(Node<T> right) {
-		this.right = right;
+	public void setRight(Node<T> rightNode) {
+		this.right = rightNode;
 	}
 
 	public T getElement() {
@@ -40,63 +42,58 @@ public class Node <T> {
 	public void setElement(T element) {
 		this.element = element;
 	}
-
+//changed left and right to leftNode and rightNode throughout the document
 	@SuppressWarnings("unchecked")
-	//For your Project 3 BST, you need to modify the addNode () method
+	//For Project 3 BST, you need to modify the addNode () method
 	//so that it will no longer allow duplicates.
+	
+	//this function follows structured programming and Edsger Dijkstra's rules of structured programming
 	public void addNode(Node <T> newNode){
 		Comparable<T> tempElement = (Comparable<T>) newNode.element;	
 		int comp = tempElement.compareTo(element);
 	      if (comp < 0)
 	      {  
-	         if (left == null) 
-	        	 {left = newNode;}
+	         if (leftNode == null) 
+	        	 {leftNode = newNode;}
 	         else 
-	        	 {left.addNode(newNode);}
+	        	 {leftNode.addNode(newNode);}
 	      }
 	     
 	      else if (comp > 0)
 	      {  
-	         if (right == null) 
-	        	 {right = newNode;}
+	         if (rightNode == null) 
+	        	 {rightNode = newNode;}
 	         else 
-	        	 {right.addNode(newNode);}
+	        	 {rightNode.addNode(newNode);}
 	      }
 	}
 	public void printNodesInOrder()
 	   {  
-	      if (left != null)
-	         {left.printNodesInOrder();}
+	      if (leftNode != null)
+	         {leftNode.printNodesInOrder();}
 	      System.out.print(element + " ");
-	      if (right != null)
-	         {right.printNodesInOrder();}
+	      if (rightNode != null)
+	         {rightNode.printNodesInOrder();}
 	   }
 	   
 	   public void printNodesPostOrder()
 	   {  
-	      if (left != null)
-	         {left.printNodesPostOrder();} 
-	      if (right != null)
-	         {right.printNodesPostOrder();}
+	      if (leftNode != null)
+	         {leftNode.printNodesPostOrder();} 
+	      if (rightNode != null)
+	         {rightNode.printNodesPostOrder();}
 	      System.out.print(element + " ");
 	   }
 	   
 	   public void printNodesPreOrder()
 	   {  
 		  System.out.print(element + " ");
-	      if (left != null)
-	         {left.printNodesPreOrder();}
-	      if (right != null)
-	         {right.printNodesPreOrder();}
+	      if (leftNode != null)
+	         {leftNode.printNodesPreOrder();}
+	      if (rightNode != null)
+	         {rightNode.printNodesPreOrder();}
 	   }
-	 // finish your Project 3 code here:
-	 //Write an Override method called toString() method here 
-/*
- * For instance, if you add nodes like: 10, 7, 15, 3, 9, 8 
- * into BinarySearchTree, when toString() method is called in 
- * a Tester class, it will print the below result to the console window:  
- *   ( (  (  .  3   .  )  7   (  (  .  8   .  )  9   .  )  )  10   (  .  15   .  ) )
- */
+
 	 
 	   public String toString(){
 		   if(element==null){
@@ -104,11 +101,11 @@ public class Node <T> {
 		   }
 		   String leftResult=".";
 		   String rightResult=".";
-		   if(left!=null){
-			   leftResult=left.toString();
+		   if(leftNode!=null){
+			   leftResult=leftNode.toString();
 		   }
-		   if(right!=null){
-			   rightResult=right.toString();
+		   if(rightNode!=null){
+			   rightResult=rightNode.toString();
 		   }
 		   return String.format("(%s %s %s)", leftResult,element.toString(),rightResult);
 	   }
